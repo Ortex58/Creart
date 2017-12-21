cb2885ddf0b861628d6c37c4c5b3ea91505bf308
Breez <ortex@i.ua>
2017-12-21 13:41:36 +0200

    Main1.1

--- js/index.js
+++ js/index.js
@@ -5,3 +5,20 @@ $('.grid').masonry({
   columnWidth: '.grid-sizer',
   percentPosition: true
 });
+
+
+$(window).scroll(function(){
+		// 200px от верха
+		if ($(window).scrollTop() > '400'){
+			//alert('200');
+			$('header').css({"background":"rgba(244, 247, 249, 0.5)"});
+		}else if ($(window).scrollTop() < '91') {
+			$('header').css({"background":"rgba(244, 247, 249, 1)"});
+		}
+		
+		// низ активного окна...
+		/*if ($(window).scrollTop() == $(document).height() - $(window).height()){
+			alert($(window).scrollTop());
+			$('#div').css({"background":"url(http://www.fonstola.ru/pic/201111/1366x768/fonstola.ru-55713.jpg);"});
+		}*/
+	});
